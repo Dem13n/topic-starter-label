d4045b67bcd693dba858f340eab7f9059e4ebc37
Dem13n <52776643+Dem13n@users.noreply.github.com>
2021-02-01 14:05:11 +0300

    fixing a small problem

--- extend.php
+++ extend.php
@@ -1,11 +1,15 @@
 <?php
 
 use Flarum\Extend;
+use Flarum\Api\Controller\ShowDiscussionController;
 
 return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__ . '/js/dist/forum.js')
         ->css(__DIR__ . '/less/forum.less'),
+
+    (new Extend\ApiController(ShowDiscussionController::class))
+        ->addInclude('user'),
 
     new Extend\Locales(__DIR__ . '/locale'),
 ];
